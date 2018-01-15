0d74722bfb35d9a3978a58c6d876c208b9e0470a
souparno majumder <souparno.majumder@gmail.com>
2018-01-15 19:30:45 +0530

    folder name and file naming convention

---
+++ src/skynet/LayerInfo.java
@@ -0,0 +1,142 @@
+package skynet;
+
+/**
+ * A simple class, designed to hold the information required to create a layer
+ * in the neural network
+ *
+ * @author bwinrich
+ */
+
+class LayerInfo {
+
+	/**
+	 * An integer for the type of activation function (see comments in
+	 * config file for details)
+	 */
+	private int activationFunction;
+
+	/**
+	 * A boolean for if the layer has a bias node or not
+	 */
+	private boolean isBiased;
+
+	/**
+	 * An integer for the number of normal neurons in the layer
+	 */
+	private int neurons;
+
+	/**
+	 * A constructor with parameters. We have no need for a default
+	 * constructor
+	 *
+	 * @param activationFunction type of activation function
+	 * @param isBiased is there a bias node
+	 * @param neurons number of normal neurons
+	 */
+	public LayerInfo(int activationFunction, boolean isBiased, int neurons) {
+		this.activationFunction = activationFunction;
+		this.isBiased = isBiased;
+		this.neurons = neurons;
+	}
+
+	/**
+	 * Accessor method for activationFunction
+	 *
+	 * @return the activationFunction
+	 */
+	public int getActivationFunction() {
+		return activationFunction;
+	}
+
+	/**
+	 * Accessor method for isBiased
+	 *
+	 * @return the isBiased
+	 */
+	public boolean isBiased() {
+		return isBiased;
+
+	}
+
+	/**
+	 * Accessor method for neurons
+	 *
+	 * @return the neurons
+	 */
+	public int getNeurons() {
+		return neurons;
+	}
+
+	/**
+	 * A method used for returning the information for the layer in an
+	 * easy-to-read format, so that it can be printed.
+	 *
+	 * @see java.lang.Object#toString()
+	 */
+	@Override
+	public String toString() {
+
+		String activation = null;
+
+		switch (activationFunction) {
+			case -1:
+				activation = "n/a";
+				break;
+			case 0:
+				activation = "Sigmoid";
+				break;
+			case 1:
+				activation = "Hyperbolic Tangent";
+				break;
+			case 2:
+				activation = "Linear";
+				break;
+			case 3:
+				activation = "Elliott";
+				break;
+			case 4:
+				activation = "Gaussian";
+				break;
+			case 5:
+				activation = "Logarithmic";
+				break;
+			case 6:
+				activation = "Ramp";
+				break;
+			case 7:
+				activation = "Sine";
+
+				break;
+			case 8:
+				activation = "Step";
+				break;
+			case 9:
+				activation = "BiPolar";
+				break;
+			case 10:
+				activation = "Bipolar Sigmoid";
+				break;
+			case 11:
+				activation = "Clipped Linear";
+				break;
+			case 12:
+				activation = "Competitive";
+				break;
+			case 13:
+				activation = "Elliott Symmetric";
+				break;
+			case 14:
+				activation = "Softmax";
+				break;
+			case 15:
+				activation = "Steepened Sigmoid";
+				break;
+			default:
+				activation = "Invalid";
+				break;
+		}
+
+		return ("Layer: (" + activation + ", " + isBiased + ", " + neurons
+			+ ")");
+	}
+}
---
+++ src/skynet/OutputWriter.java
@@ -0,0 +1,234 @@
+package skynet;
+
+import java.io.BufferedWriter;
+import java.io.File;
+import java.io.IOException;
+import org.encog.engine.network.activation.*;
+import org.encog.neural.flat.FlatNetwork;
+import org.encog.neural.networks.BasicNetwork;
+
+/**
+ * A parent class for other OutputWriters. This class holds all of the
+ *
+ * shared methods required to create a file and output the code/formula for a
+ * trained Artificial Neural Network.
+ *
+ * @author bwinrich
+ */
+public abstract class OutputWriter {
+
+	/**
+	 * The file to write to
+	 */
+	protected File file2;
+
+	/**
+	 * A BufferedWriter for file writing
+	 */
+	protected BufferedWriter bw2 = null;
+
+	/**
+	 * The name of the file
+	 */
+	protected String outputName;
+
+	/**
+	 * Does the data set have headers?
+	 */
+	protected boolean hasHeaders;
+
+	/**
+	 * Array to hold the column names (only if the data set has headers)
+	 */
+	protected String[] columnNames;
+
+	/**
+	 * The data structure for the ANN
+	 */
+	protected BasicNetwork network;
+
+	/**
+	 * The number of input nodes
+	 */
+	protected int inputCount;
+
+	/**
+	 * The number of output nodes
+	 */
+	protected int outputCount;
+
+	/**
+	 * The number of neurons in each layer (including bias neurons)
+	 */
+	protected int[] numberOfTotalNeurons;
+
+	/**
+	 * The number of neurons in each layer (excluding bias neurons)
+	 */
+	protected int[] numberOfNormalNeurons;
+
+	/**
+	 * The value of the biases of each layer, if applicable
+	 */
+	protected double[] biases;
+
+	/**
+	 * The flattened version of the ANN
+	 */
+	protected FlatNetwork myFlat;
+
+	/**
+	 * The number of layers in the ANN
+	 */
+	protected int layers;
+
+	/**
+	 * Default constructor
+	 */
+	public OutputWriter() {
+	}
+
+	/**
+	 * Mutator method for hasHeaders
+	 *
+	 * @param hasHeaders the hasHeaders to set
+	 */
+	public void setHasHeaders(boolean hasHeaders) {
+		this.hasHeaders = hasHeaders;
+	}
+
+	/**
+	 * Mutator method for columnNames
+	 *
+	 * @param columnNames the columnNames to set
+	 */
+	public void setColumnNames(String[] columnNames) {
+		this.columnNames = columnNames;
+	}
+
+	/**
+	 * Mutator method for network
+	 *
+	 * @param network the network to set
+	 */
+	public void setNetwork(BasicNetwork network) {
+		this.network = network;
+	}
+
+	/**
+	 * Mutator method for inputCount
+	 *
+	 * @param inputCount the inputCount to set
+	 */
+	public void setInputCount(int inputCount) {
+		this.inputCount = inputCount;
+	}
+
+	/**
+	 * Mutator method for outputCount
+	 *
+	 * @param outputCount the outputCount to set
+	 */
+	public void setOutputCount(int outputCount) {
+		this.outputCount = outputCount;
+	}
+
+	/**
+	 * Mutator method for numberOfTotalNeurons
+	 *
+	 * @param numberOfTotalNeurons the numberOfTotalNeurons to set
+	 */
+	public void setNumberOfTotalNeurons(int[] numberOfTotalNeurons) {
+		this.numberOfTotalNeurons = numberOfTotalNeurons;
+	}
+
+	/**
+	 * Mutator method for numberOfNormalNeurons
+	 *
+	 * @param numberOfNormalNeurons the numberOfNormalNeurons to set
+	 */
+	public void setNumberOfNormalNeurons(int[] numberOfNormalNeurons) {
+		this.numberOfNormalNeurons = numberOfNormalNeurons;
+	}
+
+	/**
+	 * Mutator method for biases
+	 *
+	 * @param biases the biases to set
+	 */
+	private void setBiases(double[] biases) {
+
+		this.biases = biases;
+	}
+
+	/**
+	 * Mutator method for myFlat
+	 *
+	 * @param myFlat the myFlat to set
+	 */
+	private void setMyFlat(FlatNetwork myFlat) {
+		this.myFlat = myFlat;
+	}
+
+	/**
+	 * Mutator method for layers
+	 *
+	 * @param layers the layers to set
+	 */
+	public void setLayers(int layers) {
+		this.layers = layers;
+	}
+
+	/**
+	 * Some variables can be initialized using information already passed to
+	 * the class.
+	 */
+	public void initializeOtherVariables() {
+		setMyFlat(network.getStructure().getFlat());
+		setBiases(myFlat.getBiasActivation());
+
+	}
+
+	/**
+	 * Creates the file used for output.
+	 *
+	 * @param output2Name the name of
+	 */
+	public abstract void createFile(String output2Name);
+
+	/**
+	 * Writes to the output file. Each String passed as a parameter is
+	 * written on its own line
+	 *
+	 * @param stuff The line to be written to the file
+	 */
+	protected void writeTwo(String stuff) {
+		try {
+			bw2.write(stuff);
+			bw2.newLine();
+		} catch (IOException e) {
+// TODO Auto-generated catch block
+
+			e.printStackTrace();
+		}
+	}
+
+	/**
+	 * Parses the equation of the activation function and returns it in
+	 * String form
+	 *
+	 * @param af The activation function to parse
+	 * @param varName The variable passed to the activation function
+	 * @param targetVarName The variable the result of the activation
+	 * function will be stored in
+	 * @return The parsed form of the activation function in String form
+	 */
+	protected abstract String parseActivationFunction(ActivationFunction af,
+		String varName, String targetVarName);
+
+	/**
+	 * A lengthy method for writing the code/formula for the neural network
+	 * to a file. Each child class will have its own implementation.
+	 */
+	public abstract void writeFile();
+}
